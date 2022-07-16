226ff2d0a1cb2a9d383933af62b0ae6a90370960
Tarikol-Islam <tuhidtitw98@gmail.com>
2022-07-16 21:31:33 +0600

    Variant filter issue resolved

--- src/product/filters.py
+++ src/product/filters.py
@@ -7,7 +7,7 @@ class ProductFilter(django_filters.FilterSet):
     title = django_filters.CharFilter(lookup_expr='icontains')
     price_from = django_filters.NumberFilter(field_name='productvariantprice__price', lookup_expr='gte')
     price_to = django_filters.NumberFilter(field_name='productvariantprice__price', lookup_expr='lte')
-    variant = django_filters.ModelChoiceFilter(field_name='productvariantprice__product_variant_two',  queryset=ProductVariant.objects.all(),widget=forms.Select(attrs={'class': 'form-control'}) )
+    variant = django_filters.ModelChoiceFilter(field_name='productvariant__variant_title',  queryset=ProductVariant.objects.all(),widget=forms.Select(attrs={'class': 'form-control'}) )
 
     class Meta:
         model = Product
--- src/product/views/product_filter.py
+++
@@ -1,8 +0,0 @@
-import django_filters
-from ..models import Product
-class ProductFilter(django_filters.FilterSet):
-    title = django_filters.CharFilter(lookup_expr='iexact')
-
-    class Meta:
-        model = Product
-        fields = ['title',]
